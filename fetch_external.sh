1f82e76db92774c0b306abebf1fe6674929355c5
Pascal Thomet <pthomet@gmail.com>
2023-09-12 19:27:25 +0200

    use official version of imgui-node-editor (develop branch)

--- fetch_external.sh
+++ fetch_external.sh
@@ -7,6 +7,6 @@ git clone -b docking https://github.com/ocornut/imgui.git
 git clone -b main https://github.com/libsdl-org/SDL.git
 
 # Clone imgui-node-editor
-#git clone -b develop https://github.com/thedmd/imgui-node-editor.git        # official version
-git clone -b fix_win_clipping https://github.com/pthom/imgui-node-editor.git # fork with fixes + workaround
+git clone -b develop https://github.com/thedmd/imgui-node-editor.git        # official version
+# git clone -b fix_win_clipping https://github.com/pthom/imgui-node-editor.git # fork with fixes + workaround
 
